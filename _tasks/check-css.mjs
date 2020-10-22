08bee0e5d48c2eb876243d97dbfa7a93c2e0c44e
Derek Lewis <DerekNonGeneric@inf.is>
2020-10-22 15:55:01 -0400

    🔧 appease ESLint

--- _tasks/check-css.mjs
+++ _tasks/check-css.mjs
@@ -1,7 +1,7 @@
 import yarnpkgShell from '@yarnpkg/shell';
 
 const scripts = {
-  'stylelint': `npx stylelint ./index.html`,
+  stylelint: `npx stylelint ./index.html`,
 };
 
 process.exitCode = await yarnpkgShell.execute(scripts['stylelint']);
