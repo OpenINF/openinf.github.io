b015af3f169b0f94017c942b55a8017aa42d16f9
Derek Lewis <DerekNonGeneric@inf.is>
2020-10-21 21:56:59 -0400

    🏗️🚮 remove html-validate from check-html task

--- _tasks/check-html.mjs
+++ _tasks/check-html.mjs
@@ -2,9 +2,7 @@ import yarnpkgShell from '@yarnpkg/shell';
 import vnu from 'vnu-jar';
 
 const scripts = {
-  'validate-html': `npx html-validate ./index.html`,
   'vnu-jar': `java -jar ${vnu} ./index.html`,
 };
 
-process.exitCode = await yarnpkgShell.execute(scripts['validate-html']);
 process.exitCode = await yarnpkgShell.execute(scripts['vnu-jar']);
