0a15b765d3e5d0aa3ef85257e28aa675688995aa
Derek Lewis <DerekNonGeneric@inf.is>
2020-10-25 02:12:25 -0400

    🏗️♻️ simplify checks

--- _tasks/check-svg.mjs
+++ _tasks/check-svg.mjs
@@ -1,8 +1,4 @@
 import yarnpkgShell from '@yarnpkg/shell';
 import vnu from 'vnu-jar';
 
-const scripts = {
-  'vnu-jar': `java -jar ${vnu} --svg ./logo.svg`,
-};
-
-process.exitCode = await yarnpkgShell.execute(scripts['vnu-jar']);
+process.exitCode = await yarnpkgShell.execute(`java -jar ${vnu} logo.svg`);
