043ddb56831387befe01267983e1e932d09e659a
Derek Lewis <DerekNonGeneric@inf.is>
2020-11-20 23:05:26 -0500

    🏗️✨ make verify-markdown task much more robust
    ️✨ add 3p remark plugins including prettier, linkchecker, spellchecker

---
+++ .remarkrc.js
@@ -0,0 +1,96 @@
+const infoStrings = [
+  'ada',
+  'bash',
+  'bibtex',
+  'c',
+  'coffee',
+  'console',
+  'cpp',
+  'csharp',
+  'diff',
+  'dts',
+  'elixir',
+  'fortran',
+  'fountain',
+  'golang',
+  'html',
+  'html+jinja',
+  'html+liquid',
+  'java',
+  'js',
+  'json',
+  'markdown',
+  'matlab',
+  'objc',
+  'pascal',
+  'perl',
+  'php',
+  'powershell',
+  'python',
+  'r',
+  'ruby',
+  'rust',
+  'tex',
+  'text',
+  'typescript',
+];
+
+var fs = require('fs');
+var strip = require('strip-comments');
+var unified = require('unified');
+
+var personal = strip(fs.readFileSync('./dictionary.txt', 'utf8'));
+
+var naturalLanguage = unified().use([
+  require('retext-english'),
+  require('retext-equality'),
+  require('retext-passive'),
+  require('retext-profanities'),
+  [require('retext-readability'), { age: 21, minWords: 8 }],
+  [
+    require('retext-simplify'),
+    { ignore: ['function', 'interface', 'maintain'] },
+  ],
+  require('retext-syntax-mentions'),
+  [
+    require('retext-spell'),
+    { dictionary: require('dictionary-en-gb'), personal: personal },
+  ],
+]);
+
+module.exports.default = {
+  plugins: [
+    require('remark-lint'),
+    // Leave this preset at the top so that it can be overridden.
+    require('remark-preset-lint-recommended'),
+    [require('remark-lint-blockquote-indentation'), 2],
+    [
+      require('remark-lint-checkbox-character-style'),
+      {
+        checked: 'x',
+        unchecked: ' ',
+      },
+    ],
+    require('remark-lint-checkbox-content-indent'),
+    [require('remark-lint-code-block-style'), 'fenced'],
+    [require('remark-lint-fenced-code-flag'), { flags: infoStrings }],
+    require('remark-lint-definition-spacing'),
+    require('remark-frontmatter'),
+
+    // Remark Lint Style Guide preset and overrides.
+    require('remark-preset-lint-markdown-style-guide'),
+    ['lint-no-file-name-mixed-case', false],
+    ['lint-no-heading-punctuation', ':.,;'],
+
+    // Third-party plugins.
+    require('remark-validate-links'),
+    require('remark-lint-maximum-line-length'),
+    require('remark-lint-are-links-valid'),
+    require('remark-lint-no-duplicate-headings-in-section'),
+    [require('remark-retext'), naturalLanguage],
+
+    // Disables all rules that conflict with Prettier. Leave this preset at the
+    // bottom so that it can't be overridden.
+    require('remark-preset-prettier'),
+  ],
+};
