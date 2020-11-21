fd6d92e59659178894f8fc613e78350c7d550e3b
Derek Lewis <DerekNonGeneric@inf.is>
2020-11-21 10:44:25 -0500

    🏗️🔧 make one script failure mean total task failure

--- _tasks/verify-markdown.mjs
+++ _tasks/verify-markdown.mjs
@@ -1,10 +1,12 @@
 import yarnpkgShell from '@yarnpkg/shell';
 
+let code = 0;
 const scripts = [
   `eslint --ext=.md .`, // validate & style-check JS code blocks
   `remark -qf .`, // check Markdown style
 ];
 
 scripts.forEach(async (v, i) => {
-  await yarnpkgShell.execute(scripts[i]);
+  code = await yarnpkgShell.execute(scripts[i]);
+  process.exitCode = code > 0 ? code : 0;
 });
