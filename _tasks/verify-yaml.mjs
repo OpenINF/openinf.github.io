433a7778c19263e12b101e12cda4219dd8eb8b0c
Derek Lewis <DerekNonGeneric@inf.is>
2020-11-27 01:31:28 +0000

    🏗️✨ add verify-yaml task

---
+++ _tasks/verify-yaml.mjs
@@ -0,0 +1,12 @@
+import yarnpkgShell from '@yarnpkg/shell';
+
+let code = 0;
+const scripts = [
+  `find . -type d \\( -name node_modules -o -name vendor \\) -prune -false -o -name '*.yml' -o -name '*.yaml' | xargs bundle exec yaml-lint`, // validate
+  `npx prettier -c {*.yml,*.yaml}`, // style-check
+];
+
+scripts.forEach(async (v, i) => {
+  code = await yarnpkgShell.execute(scripts[i]);
+  process.exitCode = code > 0 ? code : 0;
+});
