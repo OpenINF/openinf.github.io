0daac45d2e2ede31729af74b109de6f20b6a832e
Derek Lewis <DerekNonGeneric@inf.is>
2022-04-16 09:44:11 +0000

    fix: slight adjustments to post-create.fish for smoother DX

--- .devcontainer/post-create.fish
+++ .devcontainer/post-create.fish
@@ -5,34 +5,33 @@ if test -e .ruby-version
     rbenv install
 end
 
-rbenv rehash
-
-echo 'set -Ux fish_user_paths ~/.rbenv/shims/ $fish_user_paths' >> ~/.config/fish/config.fish
+echo 'set -Ux fish_user_paths ~/.rbenv/shims/ $fish_user_paths' >>~/.config/fish/config.fish
 
 source ~/.config/fish/config.fish
 
-# Install Bundler.
-gem install bundler
-
 rbenv rehash
 
-# Configure Bundler setting local gem install path to avoid permission errors.
-bundle config set --local path 'vendor/bundle'
-
-# If there's a Gemfile, then run `bundle install`.
-# It's assumed that the Gemfile will install Jekyll too.
+# If there's a Gemfile, then install Bundler and run `bundle install`.
 if test -e Gemfile
+    # Install Bundler.
+    gem install bundler
+    # Configure Bundler setting local gem install path to avoid permission errors.
+    bundle config set --local path vendor/bundle
+    # Install the dependencies specified in the Gemfile.
     bundle install
+    # Ensure latest versions of Bundler's dependencies are installed.
+    bundle update --bundler
 else
     # If there's no Gemfile, install Jekyll.
     gem install jekyll
 end
 
-# If there's a .node-version, then run `nvm install`.
+# If there's a `.node-version`, then run `nvm install`.
 if test -e .node-version
+    # Install the specified version of Node.js.
     nvm install
     npm install -g npm # ensure latest version of npm is installed
-    npm audit fix # address any vulns package manager CLI app itself
+    npm audit fix # address any vulns in package manager CLI app itself
 end
 
 # If there's a package.json, then run `npm install`.
@@ -40,4 +39,4 @@ if test package.json
     npm install
 end
 
-echo 'rbenv rehash && nvm use' >> ~/.config/fish/config.fish
+echo 'rbenv rehash && nvm use' >>~/.config/fish/config.fish
