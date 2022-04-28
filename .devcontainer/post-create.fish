cf8b2c10812fa22ec29f866080b9e72698b77dfb
Derek Lewis <DerekNonGeneric@inf.is>
2022-04-28 06:15:51 +0000

    feat(devcontainer): add sshd to allow gpgtunnel for commit signing

--- .devcontainer/post-create.fish
+++ .devcontainer/post-create.fish
@@ -43,9 +43,23 @@ echo 'rbenv rehash && nvm use' >>~/.config/fish/config.fish
 
 # Configure Git to use `gpg2`.
 echo 'git config --global gpg.program gpg2' >>~/.config/fish/config.fish
-echo no-autostart >~/.gnupg/gpg.conf
 
 echo 'set -gx GPG_TTY (tty)' >>~/.config/fish/config.fish
+
+# To fix the " gpg: WARNING: unsafe permissions on homedir
+# '/home/path/to/user/.gnupg' " error, make sure that the .gnupg directory and
+# its contents is accessibile by your user.
+chown -R (whoami) ~/.gnupg/
+
+# Also correct the permissions and access rights on the directory.
+chmod 600 ~/.gnupg/*
+chmod 700 ~/.gnupg
+
+echo no-autostart >>~/.gnupg/gpg.conf
+
+# Remove an existing Unix-domain socket file for remote port forwarding before
+# creating a new one when gpgtunnel connection is made.
+rm ~/.gnupg/S.gpg-agent
 
 echo "to enable commit signing, run"
 echo "git config --global commit.gpgsign true"
