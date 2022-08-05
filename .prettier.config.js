74e8b6dd3111d434f695b3065b25f5a44239afa2
Derek Lewis <DerekNonGeneric@inf.is>
2022-08-05 00:35:43 -0400

    fix(conf): update & rename as .prettier.config.js
    Fixes: https://github.com/openinf/openinf.github.io/issues/575
    
    Signed-off-by: Derek Lewis <DerekNonGeneric@inf.is>

---
+++ .prettier.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 80,
+  quoteProps: 'consistent',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  parser: 'typescript',
+  overrides: [
+    {
+      files: ['.eslintrc', '.prettierrc', '.renovaterc.json', '*.json'],
+      options: { parser: 'json' },
+    },
+    {
+      files: ['*.html'],
+      options: {
+        bracketSameLine: true,
+        parser: 'html',
+        singleAttributePerLine: false,
+        singleQuote: false,
+      },
+    },
+    {
+      files: ['*.md'],
+      options: {
+        parser: 'markdown',
+        proseWrap: 'always',
+      },
+    },
+  ],
+};
