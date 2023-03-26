442c3d390704ca69fc17935cb118f033b7b0db7a
Derek El <dereknongeneric@open.inf.is>
2023-03-25 18:01:53 -0700

    devenv: partly adopt Jekyll (OpenINF Community)

--- .devcontainer/post-create.fish
+++ .devcontainer/post-create.fish
@@ -2,10 +2,13 @@
 
 # If there's a .ruby-version, then run `rbenv install`.
 if test -e .ruby-version
-    rbenv install
+    rbenv install --verbose
 end
 
-echo 'set -Ux fish_user_paths ~/.rbenv/shims/ $fish_user_paths' >>~/.config/fish/config.fish
+echo 'set -Ux fish_user_paths ~/.rbenv/shims/ $fish_user_paths' >> ~/.config/fish/config.fish
+
+# To squelch yellow message re: specifying how divergent branches be reconciled.
+echo 'git config pull.rebase true' >> ~/.config/fish/config.fish # rebase
 
 source ~/.config/fish/config.fish
 
@@ -26,40 +29,52 @@ else
     gem install jekyll
 end
 
-# If there's a `.node-version`, then run `nvm install`.
-if test -e .node-version
+# If there's a `.nvmrc`, then run `nvm install`.
+if test -e .nvmrc
     # Install the specified version of Node.js.
     nvm install
-    npm install -g npm # ensure latest version of npm is installed
-    npm audit fix # address any vulns in package manager CLI app itself
 end
 
-# If there's a package.json, then run `npm install`.
+# If there's a package.json, then run `pnpm install`.
 if test package.json
-    npm install
+    corepack enable
+    corepack prepare pnpm@latest --activate
+    pnpm install
 end
 
 echo 'rbenv rehash && nvm use' >>~/.config/fish/config.fish
 
-# Configure Git to use `gpg2`.
-echo 'git config --global gpg.program gpg2' >>~/.config/fish/config.fish
+# this will populate your ~/.gnupg directory with empty keyring files
+# it will create the ~/.gnupg directory if it does not already exist (expected)
+gpg --list-keys
 
-echo 'set -gx GPG_TTY (tty)' >>~/.config/fish/config.fish
+# If there's a .gnupg directory, then perform the following setup tasks.
+if test ~/.gnupg/
+    # Configure Git to use `gpg2`.
+    echo 'git config --global gpg.program gpg2' >> ~/.config/fish/config.fish
 
-# To fix the " gpg: WARNING: unsafe permissions on homedir
-# '/home/path/to/user/.gnupg' " error, make sure that the .gnupg directory and
-# its contents is accessibile by your user.
-chown -R (whoami) ~/.gnupg/
+    echo 'set -gx GPG_TTY (tty)' >> ~/.config/fish/config.fish
 
-# Also correct the permissions and access rights on the directory.
-chmod 600 ~/.gnupg/*
-chmod 700 ~/.gnupg
+    # To fix the " gpg: WARNING: unsafe permissions on homedir
+    # '/home/path/to/user/.gnupg' " error, make sure that the .gnupg directory and
+    # its contents is accessibile by your user.
+    # chown -R (whoami) ~/.gnupg/
 
-echo no-autostart >>~/.gnupg/gpg.conf
+    # Also correct the permissions and access rights on the directory.
+    # chmod 600 ~/.gnupg/*
+    # chmod 700 ~/.gnupg
 
-# Remove an existing Unix-domain socket file for remote port forwarding before
-# creating a new one when gpgtunnel connection is made.
-rm ~/.gnupg/S.gpg-agent
+    echo no-autostart >> ~/.gnupg/gpg.conf
 
-echo "to enable commit signing, run"
-echo "git config --global commit.gpgsign true"
+    # Remove an existing Unix-domain socket file for remote port forwarding before
+    # creating a new one when gpgtunnel connection is made.
+    rm ~/.gnupg/S.gpg-agent
+
+    printf '\n%s\n\n\t%s\n\n' 'Enable commit signing:' 'git config --global commit.gpgsign true'
+end
+
+# Install moon
+# uncomment the below for moon support
+# curl -fsSL https://moonrepo.dev/install.sh | bash
+# echo 'set -Ux fish_user_paths ~/.moon/tools/moon/latest $fish_user_paths' >>~/.config/fish/config.fish
+# source ~/.config/fish/config.fish
