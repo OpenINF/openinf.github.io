18efe096753e960142164b2071dc009e86e64299
Derek Lewis <DerekNonGeneric@inf.is>
2024-03-02 18:43:24 -0500

    Update .devcontainer/post-create.fish
    Signed-off-by: Derek Lewis <DerekNonGeneric@inf.is>

--- .devcontainer/post-create.fish
+++ .devcontainer/post-create.fish
@@ -44,7 +44,7 @@ if test -e package.json
     # Same thing as running those commands given after
     # done running `pnpm setup`, but rewritten for fish shell.
     # Needed for globally-installed CLI apps & assoc. perm issues.
-    set -Ux PNPM_HOME $HOME/.local/share/pnpm"
+    set -Ux PNPM_HOME $HOME/.local/share/pnpm
     echo 'set -Ux PNPM_HOME "$HOME/.local/share/pnpm"' >> ~/.config/fish/config.fish
 
     fish_add_path -g $PNPM_HOME
