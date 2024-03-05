408ba031386551074a0ad34abcbf0ffae664b319
Restyled.io <commits@restyled.io>
2024-03-05 04:34:55 +0000

    Restyled by clang-format

--- package-scripts.js
+++ package-scripts.js
@@ -3,31 +3,31 @@
 const NPSUtils = require('nps-utils');
 
 module.exports = {
-  scripts: {
-    format: {
-      all: 'for i in _tasks/format/*.mjs; do node "$i"; done',
-      css: 'node build/tasks/format/format-css.mjs',
-      html: 'node build/tasks/format/format-html.mjs',
-      js: 'node build/tasks/format/format-js.mjs',
-      json: 'node build/tasks/format/format-json.mjs',
-      md: 'node build/tasks/format/format-md.mjs',
-      svg: 'node build/tasks/format/format-svg.mjs',
-      yaml: 'node build/tasks/format/format-yaml.mjs',
+  scripts : {
+    format : {
+      all : 'for i in _tasks/format/*.mjs; do node "$i"; done',
+      css : 'node build/tasks/format/format-css.mjs',
+      html : 'node build/tasks/format/format-html.mjs',
+      js : 'node build/tasks/format/format-js.mjs',
+      json : 'node build/tasks/format/format-json.mjs',
+      md : 'node build/tasks/format/format-md.mjs',
+      svg : 'node build/tasks/format/format-svg.mjs',
+      yaml : 'node build/tasks/format/format-yaml.mjs',
     },
-    test: 'nps verify.all',
-    verify: {
-      all: 'for i in _tasks/verify/*.mjs; do node "$i"; done',
-      css: 'node build/tasks/verify/verify-css.mjs',
-      html: 'node build/tasks/verify/verify-html.mjs',
-      js: 'node build/tasks/verify/verify-js.mjs',
-      json: 'node build/tasks/verify/verify-json.mjs',
-      md: 'node build/tasks/verify/verify-md.mjs',
-      svg: 'node build/tasks/verify/verify-svg.mjs',
-      validForEC: 'node build/tasks/verify/verify-valid-for-ec.mjs',
-      validForSC: 'node build/tasks/verify/verify-valid-for-sc.mjs',
-      yaml: 'node build/tasks/verify/verify-yaml.mjs',
+    test : 'nps verify.all',
+    verify : {
+      all : 'for i in _tasks/verify/*.mjs; do node "$i"; done',
+      css : 'node build/tasks/verify/verify-css.mjs',
+      html : 'node build/tasks/verify/verify-html.mjs',
+      js : 'node build/tasks/verify/verify-js.mjs',
+      json : 'node build/tasks/verify/verify-json.mjs',
+      md : 'node build/tasks/verify/verify-md.mjs',
+      svg : 'node build/tasks/verify/verify-svg.mjs',
+      validForEC : 'node build/tasks/verify/verify-valid-for-ec.mjs',
+      validForSC : 'node build/tasks/verify/verify-valid-for-sc.mjs',
+      yaml : 'node build/tasks/verify/verify-yaml.mjs',
     },
 
-    validate: NPSUtils.series.nps('lint', 'test', 'build'),
+    validate : NPSUtils.series.nps('lint', 'test', 'build'),
   },
 };
