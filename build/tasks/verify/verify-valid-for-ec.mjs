e135378df3712226ff2e5be6744189217df8abac
Derek Lewis <dereknongeneric@open.inf.is>
2024-03-13 00:04:53 -0400

    temp fix ec task
    this is not our final form!

--- build/tasks/verify/verify-valid-for-ec.mjs
+++ build/tasks/verify/verify-valid-for-ec.mjs
@@ -1,5 +1,19 @@
-import spawn from 'cross-spawn';
+import { execute } from '@yarnpkg/shell';
+// import { echoTaskRunning } from '../util.mjs';
 
-const result = await spawn('npx', ['ec'], { stdio: 'inherit' });
+// echoTaskRunning('verify-ec-harmony', import.meta.url);
 
-process.exitCode = result.exitCode;
+let exitCode = 0;
+const scripts = ["editorconfig-checker -config '.ecrc.json'"];
+
+for await (const element of scripts) {
+  try {
+    exitCode = await execute(`pnpm exec ${element}`);
+  } catch (p) {
+    exitCode = p.exitCode;
+  }
+  process.exitCode = exitCode > 0 ? exitCode : 0;
+}
+
+// eslint-disable-next-line unicorn/no-process-exit
+process.exit(0);
