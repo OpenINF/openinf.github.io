704600f092eda6de23f88a092e53fcf6b32d7a22
Derek Lewis <DerekNonGeneric@inf.is>
2024-03-19 01:30:01 -0400

    Update tools/devcontainer/post-start.fish
    Signed-off-by: Derek Lewis <DerekNonGeneric@inf.is>

--- tools/devcontainer/post-start.fish
+++ tools/devcontainer/post-start.fish
@@ -49,7 +49,7 @@ echo 'set -gx DPRINT_HOME $DPRINT_INSTALL/bin' >> $HOME/.config/fish/config.fish
 fish_add_path -g $DPRINT_HOME
 echo 'fish_add_path -g $DPRINT_HOME' >> $HOME/.config/fish/config.fish
 
-# Add the dir to path in which the executables of local node deps are linked.
+# Add the dir in which the executables of local Node deps are linked to the user's $PATH.
 echo 'fish_add_path -g (pnpm bin)' >> $HOME/.config/fish/config.fish
 
 ########################################################################
