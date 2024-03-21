0142af025eae4d24128b5fd570790fc45688c7ef
Derek Lewis <dereknongeneric@open.inf.is>
2024-03-21 00:21:32 -0400

    add autoformatting task

--- build/tasks/format/format-md.mjs
+++ build/tasks/format/format-md.mjs
@@ -1,11 +1,38 @@
-import yarnpkgShell from '@yarnpkg/shell';
+/**
+ * @file Format Markdown files to adhere to autofixable style guidelines.
+ * @author The OpenINF Authors & Friends
+ * @module {ES6Module} build/tasks/format/format-md.mjs
+ */
 
-let code = 0;
-const scripts = ['npx prettier --write "**/*.md"'];
+// -----------------------------------------------------------------------------
+// Requirements
+// -----------------------------------------------------------------------------
 
-console.log('\r\nAutoformatting all Markdown files using Prettier\r\n');
+import { execute } from '@yarnpkg/shell';
+import { $ } from 'zx';
 
-scripts.forEach(async (v, i) => {
-  code = await yarnpkgShell.execute(scripts[i]);
-  process.exitCode = code > 0 ? code : 0;
-});
+import { echoTaskRunning } from '../util.mjs';
+
+echoTaskRunning('format-md', import.meta.url);
+
+const MarkdownObject =
+  await $`bundle exec github-linguist --breakdown --json | jq '.Markdown.files'`;
+const MarkdownFiles = JSON.parse(MarkdownObject.stdout);
+
+let exitCode = 0;
+const scripts = [
+  // fix style of JS/TS code blocks in Markdown
+  `eslint --fix ${MarkdownFiles.join(' ')}`,
+  'prettier --write **/*{.*.md,.md}', // Markdown fix sty;e
+  // validate Markdown
+  'markdownlint-cli2-fix "**/**.md" "#node_modules" "#vendor"',
+];
+
+for await (const element of scripts) {
+  try {
+    exitCode = await execute(`pnpm exec ${element}`);
+  } catch (p) {
+    exitCode = p.exitCode;
+  }
+  process.exitCode = exitCode > 0 ? exitCode : 0;
+}
