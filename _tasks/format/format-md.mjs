024fb655c1a5bfda770fea778c2a19703e8f001f
Derek Lewis <dereknongeneric@open.inf.is>
2024-03-28 22:22:51 -0400

    rm misc tasking (obsoleted)
    Signed-off-by: Derek Lewis <dereknongeneric@open.inf.is>

--- _tasks/format/format-md.mjs
+++
@@ -1,9 +0,0 @@
-import yarnpkgShell from '@yarnpkg/shell';
-
-let code = 0;
-const scripts = ['npx prettier --write "**/*.md"'];
-
-scripts.forEach(async (v, i) => {
-  code = await yarnpkgShell.execute(scripts[i]);
-  process.exitCode = code > 0 ? code : 0;
-});
