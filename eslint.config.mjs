c27015ba18e1cf06b61cf81a7437025526d326f0
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-05 23:41:17 -0400

    impl eslint-plugin-markdown

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,32 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
 
+
 export default [
 	{languageOptions: {globals: globals.browser}},
 	...compat.extends('xo'),
+
+  // applies to all JavaScript files
+  {
+      rules: {
+          strict: "error"
+      }
+  },
+
+  // applies to Markdown files
+  {
+      files: ["**/*.md"],
+      plugins: {
+          markdown
+      },
+      processor: "markdown/markdown"
+  },
+
+  // applies only to JavaScript blocks inside of Markdown files
+  {
+      files: ["**/*.md/*.js"],
+      rules: {
+          strict: "off"
+      }
+  }
 ];
