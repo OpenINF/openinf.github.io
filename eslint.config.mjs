6bb092aaab18d4bd6e05be648a52f29c4ff9df5f
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-05 23:36:32 -0400

    Update eslint.config.mjs
    Signed-off-by: Derek Lewis <DerekNonGeneric@inf.is>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,19 @@
+/**
+ * @file ESLint configuration (flat format).
+ *
+ * @author The OpenINF Authors & Friends
+ * @module {type ES6Module} /eslint.config
+ * @license MIT OR Apache-2.0 OR BlueOak-1.0.0
+ * @see https://eslint.org/docs/latest/use/configure/plugins
+ */
+
+// -----------------------------------------------------------------------------
+// Requirements
+// -----------------------------------------------------------------------------
+
 import globals from 'globals';
+
+import markdown from 'eslint-plugin-markdown';
 
 import path from 'path';
 import {fileURLToPath} from 'url';
