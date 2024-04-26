e38d33d5b50f581990698d3d629723e5af240996
Derek Lewis <dereknongeneric@open.inf.is>
2024-04-26 01:38:53 -0400

    🏗✨：add robust css/scss check tasks #46
    PR URL: https://github.com/OpenINF/open.inf.is/pull/46
    Reviewed-by: OpenINF-bot <openinfbot@open.inf.is>
    
    ---------
    
    Signed-off-by: Derek Lewis <dereknongeneric@open.inf.is>

--- build/tasks/format/format-css.mjs
+++ build/tasks/format/format-css.mjs
@@ -1,9 +1,33 @@
-import yarnpkgShell from '@yarnpkg/shell';
+/**
+ * @file Format CSS/SCSS files to adhere to autofixable style guidelines.
+ * @author The OpenINF Authors & Friends
+ * @license MIT OR Apache-2.0 OR BlueOak-1.0.0
+ * @module {type ES6Module} build/tasks/format/format-css
+ */
 
-let code = 0;
-const scripts = ['npx stylelint --fix _includes/head.html'];
+import { exec, glob } from '@openinf/portal/build/utils';
 
-scripts.forEach(async (v, i) => {
-  code = await yarnpkgShell.execute(scripts[i]);
-  process.exitCode = code > 0 ? code : 0;
-});
+const SCSSFiles = await glob([
+  '**.scss',
+  '!_sass/bootstrap/',
+  '!_sass/bourbon/',
+  '!_site/',
+  '!node_modules/',
+  '!vendor/',
+]);
+
+let exitCode = 0;
+const scripts = [
+  `prettier --write ${SCSSFiles.join(' ')}`,
+  `stylelint --fix ${SCSSFiles.join(' ')}`,
+];
+
+for (const element of scripts) {
+  try {
+    exitCode = await exec(element);
+  } catch (p) {
+    exitCode = p.exitCode;
+  }
+
+  if (exitCode !== 0) process.exitCode = exitCode;
+}
--- build/tasks/verify/verify-css.mjs
+++ build/tasks/verify/verify-css.mjs
@@ -1,5 +1,33 @@
-import yarnpkgShell from '@yarnpkg/shell';
+/**
+ * @file Verify CSS/SCSS files are valid & adhere to checkable style guidelines.
+ * @author The OpenINF Authors & Friends
+ * @license MIT OR Apache-2.0 OR BlueOak-1.0.0
+ * @module {type ES6Module} build/tasks/verify/verify-css
+ */
 
-process.exitCode = await yarnpkgShell.execute(
-  'npx stylelint _includes/head.html'
-);
+import { exec, glob } from '@openinf/portal/build/utils';
+
+const SCSSFiles = await glob([
+  '**.scss',
+  '!_sass/bootstrap/',
+  '!_sass/bourbon/',
+  '!_site/',
+  '!node_modules/',
+  '!vendor/',
+]);
+
+let exitCode = 0;
+const scripts = [
+  `prettier --check ${SCSSFiles.join(' ')}`,
+  `stylelint ${SCSSFiles.join(' ')}`,
+];
+
+for (const element of scripts) {
+  try {
+    exitCode = await exec(element);
+  } catch (p) {
+    exitCode = p.exitCode;
+  }
+
+  if (exitCode !== 0) process.exitCode = exitCode;
+}
