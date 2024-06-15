e17ffebe6d89381db508ad2aa7b3c71b9cd0ab1d
Derek Lewis <dereknongeneric@open.inf.is>
2024-06-15 19:09:51 -0400

    use ts-node over esm
    Refs: https://github.com/TypeStrong/ts-node/blob/ddb05ef23be92a90c3ecac5a0220435c65ebbd2a/README.md#programmatic

--- .env-cmdrc.js
+++ .env-cmdrc.js
@@ -1,3 +1,2 @@
-// biome-ignore lint/suspicious/noGlobalAssign: <explanation>
-require = require('esm')(module /* , options */);
-module.exports = require('./.config/env-cmd').default;
+require('ts-node').register({});
+module.exports = require('./.config/env-cmd.mts').default;
