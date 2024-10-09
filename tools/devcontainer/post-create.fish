5cf988325e404ac1326d238f153087a0aadbe486
Derek Lewis <dereknongeneric@open.inf.is>
2024-10-08 20:18:59 -0400

    🏗️♻️：rm all ruby infra

--- tools/devcontainer/post-create.fish
+++ tools/devcontainer/post-create.fish
@@ -17,31 +17,5 @@ echo 'set -gx SHELL fish' >> $HOME/.config/fish/config.fish
 
 # ------------------------------------------------------------------------------
 
-# If there's a `.ruby-version`, then run `rbenv install`.
-if test -e .ruby-version
-    # Upgrade rbenv ruby-build plugin for latest build definitions.
-    git -C $HOME/.rbenv/plugins/ruby-build pull
-    # Install the specified versions/Ruby runtime!
-    rbenv install --verbose
-end
-
-echo 'fish_add_path -g $HOME/.rbenv/shims/' >> $HOME/.config/fish/config.fish
-
 source $HOME/.config/fish/config.fish
 
-rbenv rehash
-
-# If there's a Gemfile, then install Bundler and run `bundle install`.
-if test -e Gemfile
-    # Latest version already installed?
-    gem update --system -N --no-document
-    # Install Bundler.
-    gem install bundler
-    # Install the dependencies specified in the Gemfile.
-    bundle install
-    # Ensure latest versions of Bundler's dependencies are installed.
-    bundle update --bundler
-else
-    # If there's no Gemfile, install Jekyll.
-    gem install jekyll
-end
